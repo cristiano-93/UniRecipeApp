0dd0f80d9eabfd65cf5dffd3accc58bc8e0bc925
Cristiano <cristiano_santos93@hotmail.com>
2021-01-14 18:48:12 +0000

    18th try

--- controllers/home.js
+++ controllers/home.js
@@ -1,14 +1,14 @@
 const List = require('../models/List');
 
 exports.list = async (req, res) => {
-    console.log(req.session);
+    console.log(req.session+"req.session working");
     try {
-        const totalList = await List.find({}).count();
+        const totalList = await List.find({});              //.count() removed
         res.render("index", {totalList: totalList})
     } catch (e) {
         console.log(e);
         res.status(404).send({
-            message:`error rendering page`,
+            message:`error rendering page, home controller`,
         });
     }
     
